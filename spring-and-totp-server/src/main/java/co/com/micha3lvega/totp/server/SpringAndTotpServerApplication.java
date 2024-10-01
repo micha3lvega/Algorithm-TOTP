074b5021f6ca0d3d3e0a60ca140c1fb1218cda2a
Michael Vega <micha3lvega@gmail.com>
2024-10-01 17:00:46 -0500

    Inicializar proyecto servidor

---
+++ spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/SpringAndTotpServerApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.totp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndTotpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndTotpServerApplication.class, args);
+	}
+
+}
---
+++ spring-and-totp-server/src/test/java/co/com/micha3lvega/totp/server/SpringAndTotpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.totp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndTotpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
