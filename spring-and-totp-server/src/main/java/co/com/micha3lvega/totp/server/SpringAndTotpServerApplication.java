ca0e82cedbe30df608ed4f10522e3ea99785b11d
Michael Vega <micha3lvega@gmail.com>
2024-10-05 21:02:05 -0500

    Desactivar seguridad sobre las paginas

--- spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/SpringAndTotpServerApplication.java
+++ spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/SpringAndTotpServerApplication.java
@@ -2,8 +2,9 @@ package co.com.micha3lvega.totp.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringAndTotpServerApplication {
 
 	public static void main(String[] args) {
