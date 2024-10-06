6f2174d52ba41d2f2156724b92b6e70d4ba9a243
Michael Vega <micha3lvega@gmail.com>
2024-10-05 19:13:26 -0500

    Corregir error de generación del secretkey

--- spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/util/TotpSecretKeyGenerator.java
+++ spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/util/TotpSecretKeyGenerator.java
@@ -1,21 +1,33 @@
 package co.com.micha3lvega.totp.server.util;
 
 import java.security.SecureRandom;
-import java.util.Base64;
 
+import org.jboss.aerogear.security.otp.api.Base32;
+
+/**
+ * Generador de claves secretas para TOTP (Time-based One-Time Password). Esta clase proporciona un método para generar una clave secreta aleatoria que puede ser utilizada en el proceso de autenticación TOTP.
+ */
 public class TotpSecretKeyGenerator {
 
-	private static final int SECRET_KEY_LENGTH = 40;
+	private static final int SECRET_KEY_LENGTH = 16;
 
+	/**
+	 * Constructor privado para evitar la instanciación de la clase. Lanza una excepción si se intenta instanciar.
+	 */
 	private TotpSecretKeyGenerator() {
 		throw new IllegalStateException("Utility class");
 	}
 
+	/**
+	 * Genera una clave secreta aleatoria de 16 bytes.
+	 *
+	 * @return una cadena que representa la clave secreta en formato Base32.
+	 */
 	public static String generateSecretKey() {
 		var secureRandom = new SecureRandom();
 		var randomBytes = new byte[SECRET_KEY_LENGTH];
 		secureRandom.nextBytes(randomBytes);
-		return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+		return Base32.encode(randomBytes);
 	}
 
 }
