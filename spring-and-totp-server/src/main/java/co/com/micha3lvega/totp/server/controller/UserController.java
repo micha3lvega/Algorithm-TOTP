26f5d21e1449f6bbcbe4d4eb740bd729a8a0672a
Michael Vega <micha3lvega@gmail.com>
2024-10-10 22:48:00 -0500

    Crear controlador para login

--- spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/controller/UserController.java
+++ spring-and-totp-server/src/main/java/co/com/micha3lvega/totp/server/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.com.micha3lvega.totp.server.dto.SignUpDTO;
+import co.com.micha3lvega.totp.server.dto.SingInDTO;
 import co.com.micha3lvega.totp.server.model.User;
 import co.com.micha3lvega.totp.server.services.UserServices;
 import jakarta.validation.Valid;
@@ -29,6 +30,13 @@ public class UserController {
 		user = userServices.createUser(user);
 		user.setPassword(null); // no retornar el password
 		return user;
+
+	}
+
+	@PostMapping("/login")
+	public User login(@RequestBody @Valid SingInDTO singIn) {
+
+		return userServices.login(singIn.getUsername(), singIn.getPassword());
 
 	}
 
