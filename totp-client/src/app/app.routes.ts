4aa4ad76c9e68f7649c63cbc282e2caedacb3707
Michael Vega <micha3lvega@gmail.com>
2024-10-10 22:38:46 -0500

    Crear rutas

--- totp-client/src/app/app.routes.ts
+++ totp-client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupFormComponent },
+];
