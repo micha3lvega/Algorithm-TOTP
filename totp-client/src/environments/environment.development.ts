955cb2d31c02e174f937d635dfdcbfc3d18fbf62
Michael Vega <micha3lvega@gmail.com>
2024-10-11 14:31:19 -0500

    Crear variables de entorno

---
+++ totp-client/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  ivText: 'dev',
+  secretKeyText: 'dev'
+};
---
+++ totp-client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ivText: 'prod',
+  secretKeyText: 'prod',
+};
